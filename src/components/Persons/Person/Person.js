71a6220509e5ce15fe746440dc59f93ba5be7c19
betachecks <betachecksafrica@gmail.com>
2021-03-30 14:09:59 +0100

    learning react

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 //import styled from 'styled-components';
 import classes from './Person.css';
+import Auxiliary from '../../../hoc/Auxiliary';
 
 // const StyleDiv = styled.div`
 //         width: 60%;
@@ -27,15 +28,13 @@ class Person extends Component {
     // }
     render() {
         console.log('[Person.js] rendering...');
+        //<div className="Person" style={style}>
         return (
-            //<div className="Person" style={style}>
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I am a {this.props.name} and i am {this.props.age} years old</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
-
-
+            <Auxiliary>
+                <p key='i1' onClick={this.props.click}>I am a {this.props.name} and i am {this.props.age} years old</p>
+                <p key='i2'>{this.props.children}</p>
+                <input key='13' type="text" onChange={this.props.changed} value={this.props.name} />
+            </Auxiliary>
         );
     }
 }
